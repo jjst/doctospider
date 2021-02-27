acc0d3735aceb7e74c82dfd04ef06b2083c0ff0e
Jeremie Jost <jeremie.jost@zalando.ie>
2021-02-27 19:35:16 +0100

    Add twilio wiring

--- spider.py
+++ spider.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
 import re
 
+import os
 import requests
 from bs4 import BeautifulSoup
 import re
+from twilio.rest import Client
 
-URL = "https://www.doctolib.fr/vaccination-covid-19/rosheim?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005"
+DOCTOLIB_SEARCH_URL = "https://www.doctolib.fr/vaccination-covid-19/rosheim?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005"
+
+
+# Find these values at https://twilio.com/user/account
+# To set up environmental variables, see http://twil.io/secure
+try:
+    twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    twilio_target_number = os.environ['TWILIO_TARGET_NUMBER']
+    twilio_client = Client(account_sid, auth_token)
+except KeyError:
+    print("Environment variables for Twilio not found. I won't be able to send texts!")
 
 
 def fetch_appointments():
-    response = requests.get(URL)
+    response = requests.get(DOCTOLIB_SEARCH_URL)
     soup = BeautifulSoup(response.text)
     results = soup.select(".dl-search-result")
     for result in results:
@@ -23,6 +36,12 @@ def fetch_appointments():
         print(f"Found {avail_count} available spots for location: {place_name}")
         if avail_count:
             print("I found a spot at {place_name}! Sending notification.")
+            message_body = "Bonjour, il y a un RDV disponible au centre de vaccination {place_name} sur doctolib.fr"
+            twilio_client.api.account.messages.create(
+                to=twilio_target_number,
+                from_=twilio_from,
+                body=message_body
+            )
 
 
 
