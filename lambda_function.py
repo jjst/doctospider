7db0f15077b3bc3c040c5b37ad85a20dca78c3ca
Jeremie Jost <jeremie.jost@zalando.ie>
2021-02-27 18:57:47 +0100

    Remove dead code

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,5 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
-from spider import DaftSpider
+from spider import fetch_appointments
 
 
 def lambda_handler(event, context):
-    process = CrawlerProcess(settings={
-        'FEED_FORMAT': 'json',
-        'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0'
-    })
-
-    process.crawl(DaftSpider)
-    process.start()
+    fetch_appointments()
